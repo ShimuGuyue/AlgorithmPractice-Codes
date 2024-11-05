5bb1e4b85d2a8551362acc4f7c2b5e5bf51020eb
ShimuGuyue <2147539031@qq.com>
2024-11-05 18:54:32 +0800

    codes_for_contests

---
+++ .Codes_for_Contests/G.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+namespace solve
+{
+	void Solve(void);
+}
+
+int main(void)
+{
+	int t(1);
+	//std::cin >> t;
+	while (t--)
+	{
+		solve::Solve();
+		//std::cout << std::endl;
+	}
+	return 0;
+}
+
+void solve::Solve(void)
+{
+    
+}
