7b254c49d40dbdfa345daf95cf0c24995d5ce768
ShimuGuyue <2147539031@qq.com>
2024-11-05 18:12:34 +0800

    洛谷P1094

--- luogu/Test_Codes/_DEBUG.cpp
+++ luogu/Test_Codes/_DEBUG.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
 int main()
 {
-    
+    std::vector<int> as(10);
+    for (int i(0); i < 10; ++i)
+    {
+        std::cin >> as[i];
+    }
+    std::sort(as.rbegin(), as.rend());
+    for (int i(0); i < 10; ++i)
+    {
+        std::cout << as[i] << " ";
+    }
     return 0;
 }
