71b6ffbe878616902a71862cec531cef6afea214
ShimuGuyue <2147539031@qq.com>
2024-11-26 22:20:04 +0800

    luogu p2234

--- LuoGu/Test_Codes/_DEBUG.cpp
+++ LuoGu/Test_Codes/_DEBUG.cpp
@@ -1,30 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
 int main()
 {
-	char a[2000];
-	int b[26] = {0};
-	int i = 0;
-	scanf("%s", a);
-	for (int i(0); i < strlen(a); ++i)
-	{
-		int j(a[i] - 'a');
-		b[j]++;
-	}
-	// while (scanf("%c", &a[i]) != '\n')
-	// {
-	// 	int j = a[i] - 'a';
-	// 	b[j]++;
-	// 	i++;
-	// }
-	for (int k = i - 1; k > 0; k--)
-	{
-		if (b[k] > b[k - 1])
-		{
-			int c = b[k];
-			b[k] = b[k - 1];
-			b[k - 1] = c;
-		}
-	}
-	printf("%d", b[0]);
+	std::cout << INT_MAX << std::endl;
+	std::cout << INT_MIN << std::endl;
+	std::cout << 0x7fffffff << std::endl;
 }
