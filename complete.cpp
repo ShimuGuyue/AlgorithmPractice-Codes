5fa2afa894fce8a2d53690d764ad8fc1fe5d4cb8
ShimuGuyue <2147539031@qq.com>
2024-12-09 18:15:05 +0800

    .

--- complete.cpp
+++ complete.cpp
@@ -8,7 +8,7 @@ int main(void)
 	freopen(".in.in"  , "r", stdin );
 	freopen(".out.out", "w", stdout);
 	#endif
-	
+		
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL); std::cout.tie(NULL);
 
