fbd279c3219201642aaeb1d631518d4b22db8258
ShimuGuyue <2147539031@qq.com>
2024-12-17 18:06:35 +0800

    cf 1352 abcd

--- Codeforces/Test_Codes/CF1352D.cpp
+++ Codeforces/Test_Codes/CF1352D.cpp
@@ -38,10 +38,50 @@ void Solve(void)
 	{
 		std::cin >> as[i];
 	}
-	int32_t flag = 0;
-	int i = 0, j = n - 1;
+	int32_t count = 0;
+	int32_t i = 0, j = n - 1;
+	int32_t sum_a = 0, sum_b = 0;
+	int32_t a = 0, b = 0;
 	while (1)
 	{
-		
+		int32_t sum = 0;
+		bool ok = true;
+		while (sum <= b)
+		{
+			if (i > j)
+			{
+				ok = false;
+				break;
+			}
+			sum += as[i];
+			++i;
+		}
+		if (sum)
+			++count;
+		a = sum;
+		sum_a += a;
+		if (!ok)
+			break;
+
+		sum = 0;
+		ok = true;
+		while (sum <= a)
+		{
+			if (j < i)
+			{
+				ok = false;
+				break;
+			}
+			sum += as[j];
+			--j;
+		}
+		if (sum)
+			++count;
+		b = sum;
+		sum_b += b;
+		if (!ok)
+			break;
 	}
+	
+	std::cout << count << " " << sum_a << " " << sum_b << std::endl;
 }
