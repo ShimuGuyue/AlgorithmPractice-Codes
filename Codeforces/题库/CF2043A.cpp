83fd82ca6f8095d2b2ac45382d7b3eca8c2f9c48
ShimuGuyue <2147539031@qq.com>
2024-12-27 13:54:44 +0800

    Educational Codeforces Round 173 (Rated for Div. 2)

---
+++ Codeforces/题库/CF2043A.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdint>
+
+void Solve(void);
+
+int main(void)
+{
+	#ifdef Shimu_Guyue
+		freopen(".in.in"  , "r", stdin );
+		freopen(".out.out", "w", stdout);
+		#define RUN_TIME \
+			std::clog << (clock() * 1.0 / CLOCKS_PER_SEC) * 1000 << " ms" << std::endl;
+	#else
+		#define RUN_TIME
+	#endif
+
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL); std::cout.tie(NULL);
+
+    int t = 1;
+	std::cin >> t;
+	while (t--)
+	{
+		Solve();
+		//std::cout << std::endl;
+	}
+	RUN_TIME
+	return 0;
+}
+
+void Solve(void)
+{
+	int64_t n;
+	std::cin >> n;
+	int64_t ans = 1;
+	while (n > 3)
+	{
+		n /= 4;
+		ans *= 2;
+	}
+	std::cout << ans << std::endl;
+}
