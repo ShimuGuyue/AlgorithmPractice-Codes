c10fd4291b18aa1e2d4f4ba95b7870aeaee3b369
ShimuGuyue <2147539031@qq.com>
2025-01-05 18:34:10 +0800

    洛谷 p4913

--- LuoGu/题单/【数据结构1-2】二叉树/P4913.cpp
+++ LuoGu/题单/【数据结构1-2】二叉树/P4913.cpp
@@ -1,3 +1,17 @@
+/**
+ * 	80 分 WA #2 ：试试这组样例
+ * 
+ * 	7
+ * 	4 5
+ * 	3 7
+ * 	0 0
+ * 	2 6
+ * 	0 0
+ * 	0 0
+ * 	0 0
+ * 
+ * 	4
+ */
 #include <iostream>
 #include <iomanip>
 #include <cstdint>
@@ -27,6 +41,16 @@ using std::sort;
 using std::max_element;
 using std::min_element;
 
+struct Node
+{
+	int left;
+	int right;
+};
+
+vector<Node> tree;
+int ans;
+
+void Dfs(Node &node, int depth);
 void Solve(void);
 
 int main(void)
@@ -54,5 +78,28 @@ int main(void)
 
 void Solve(void)
 {
+	int n;
+	cin >> n;
+	tree = vector<Node>(n + 1);
+	for (int i = 1; i <= n; ++i)
+	{
+		int l, r;
+		cin >> l >> r;
+		tree[i].left = l;
+		tree[i].right = r;
+	}
+	// int ans = 0;
+	ans = 0;
+	Dfs(tree[1], 1);
+	cout << ans << endl;
+}
 
+void Dfs(Node &node, int depth)
+{
+	ans = std::max(ans, depth);
+
+	if (node.left != 0)
+		Dfs(tree[node.left], depth + 1);
+	if (node.right != 0)
+		Dfs(tree[node.right], depth + 1);
 }
