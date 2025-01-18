27fa3f77c717f4000f4cc5dd8e78e46bd4641108
ShimuGuyue <2147539031@qq.com>
2025-01-18 14:51:11 +0800

    cf2056A

---
+++ Codeforces/题库/CF2056A.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::pair;
+using std::string;
+using std::vector;
+using std::deque;
+using std::queue;
+using std::stack;
+using std::set;
+using std::map;
+using std::sort;
+using std::max_element;
+using std::min_element;
+
+void Solve(void);
+
+int main(void)
+{
+	#ifdef Shimu_Guyue
+		freopen("test.in" , "r", stdin );
+		freopen("test.out", "w", stdout);
+	#else
+	#endif
+
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t = 1;
+	cin >> t;
+	while (t--)
+	{
+		Solve();
+		// cout << endl;
+	}
+	return 0;
+}
+
+void Solve(void)
+{
+	int n, m;
+	cin >> n >> m;
+	int lx = m, ly = m;
+	int tempxy;
+	cin >> tempxy >> tempxy;
+	while (--n)
+	{
+		int x, y;
+		cin >> x >> y;
+		lx += x;
+		ly += y;
+	}
+	cout << (lx + ly) * 2 << endl;
+}
