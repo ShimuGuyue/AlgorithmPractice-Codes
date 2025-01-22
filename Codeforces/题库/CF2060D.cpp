fe7633812798db8292cc94e7b4c32d4d7cdde202
ShimuGuyue <2147539031@qq.com>
2025-01-22 13:54:49 +0800

    Codeforces Round 998 (Div. 3) ABCD

---
+++ Codeforces/题库/CF2060D.cpp
@@ -0,0 +1,88 @@
+//  $$$$$$\  $$\   $$\     $$\  $$$$$$\  $$$$$$\  $$$$$$\  
+// $$  __$$\ $$ |  \$$\   $$  |$$  __$$\ \_$$  _|$$  __$$\ 
+// $$ /  $$ |$$ |   \$$\ $$  / $$ /  \__|  $$ |  $$ /  $$ |
+// $$$$$$$$ |$$ |    \$$$$  /  \$$$$$$\    $$ |  $$$$$$$$ |
+// $$  __$$ |$$ |     \$$  /    \____$$\   $$ |  $$  __$$ |
+// $$ |  $$ |$$ |      $$ |    $$\   $$ |  $$ |  $$ |  $$ |
+// $$ |  $$ |$$$$$$$$\ $$ |    \$$$$$$  |$$$$$$\ $$ |  $$ |
+// \__|  \__|\________|\__|     \______/ \______|\__|  \__|
+// 你是否赞同纯美女神爱莉希雅的美貌盖世无双？【愛門】!
+#include <iostream>
+#include <iomanip>
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::pair;
+using std::string;
+using std::vector;
+using std::deque;
+using std::queue;
+using std::stack;
+using std::priority_queue;
+using std::set;
+using std::map;
+using std::sort;
+using std::max_element;
+using std::min_element;
+
+void Solve(void);
+
+int main(void)
+{
+	#ifdef Shimu_Guyue
+		freopen("test.in" , "r", stdin );
+		freopen("test.out", "w", stdout);
+	#else
+	#endif
+
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t = 1;
+	cin >> t;
+	while (t--)
+	{
+		Solve();
+		// cout << endl;
+	}
+	return 0;
+}
+
+void Solve(void)
+{
+	int n;
+	cin >> n;
+	vector<int> as(n);
+	for (int &a : as)
+	{
+		cin >> a;
+	}
+	for (int i = 1; i < n; ++i)
+	{
+		int min = std::min(as[i], as[i - 1]);
+		as[i] -= min;
+		as[i - 1] -= min;
+	}
+	for (int i = 1; i < n; ++i)
+	{
+		if (as[i - 1] > as[i])
+		{
+			cout << "NO" << endl;
+			return;
+		}
+	}
+	cout << "YES" << endl;
+}
