36a66e40f9afcf3e83540c3b3e66f837701b16a4
ShimuGuyue <2147539031@qq.com>
2025-01-25 21:59:19 +0800

    创建 NC16593.cpp

---
+++ NowCoder/题库/NC16593.cpp
@@ -0,0 +1,90 @@
+//  $$$$$$\  $$\   $$\     $$\  $$$$$$\  $$$$$$\  $$$$$$\  
+// $$  __$$\ $$ |  \$$\   $$  |$$  __$$\ \_$$  _|$$  __$$\ 
+// $$ /  $$ |$$ |   \$$\ $$  / $$ /  \__|  $$ |  $$ /  $$ |
+// $$$$$$$$ |$$ |    \$$$$  /  \$$$$$$\    $$ |  $$$$$$$$ |
+// $$  __$$ |$$ |     \$$  /    \____$$\   $$ |  $$  __$$ |
+// $$ |  $$ |$$ |      $$ |    $$\   $$ |  $$ |  $$ |  $$ |
+// $$ |  $$ |$$$$$$$$\ $$ |    \$$$$$$  |$$$$$$\ $$ |  $$ |
+// \__|  \__|\________|\__|     \______/ \______|\__|  \__|
+// 你是否赞同纯美女神爱莉希雅的美貌盖世无双？【愛門】!
+#include <iostream>
+#include <iomanip>
+#include <cstdint>
+#include <climits>
+#include <string>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::pair;
+using std::string;
+using std::vector;
+using std::deque;
+using std::queue;
+using std::stack;
+using std::priority_queue;
+using std::set;
+using std::map;
+using std::sort;
+using std::max_element;
+using std::min_element;
+
+void Solve(void);
+
+int main(void)
+{
+	#ifdef Shimu_Guyue
+		freopen("test.in" , "r", stdin );
+		freopen("test.out", "w", stdout);
+	#else
+	#endif
+
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t = 1;
+	// cin >> t;
+	while (t--)
+	{
+		Solve();
+		// cout << endl << "-------------------------" << endl;
+	}
+	return 0;
+}
+
+void Solve(void)
+{
+    struct Order
+    {
+        int a, b, g, k;
+    };
+    
+
+    int n;
+    cin >> n;
+    vector<Order> orders(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> orders[i].a >> orders[i].b >> orders[i].g >> orders[i].k;
+    }
+    int x, y;
+    cin >> x >> y;
+    int ans = -1;
+    for (int i = 0; i < n; ++i)
+    {
+        Order &o = orders[i];
+        if (o.a <= x && x <= o.a + o.g && o.b <= y && y <= o.b + o.k)
+            ans = i + 1;
+    }
+    cout << ans << endl;
+}
