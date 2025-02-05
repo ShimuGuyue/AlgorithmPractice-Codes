147f11dc3012db875ad83fb163e21409ffcea98c
ShimuGuyue <2147539031@qq.com>
2025-02-05 17:59:22 +0800

    创建 NC207040.cpp

---
+++ NowCoder/题库/NC207040.cpp
@@ -0,0 +1,101 @@
+//  $$$$$$\  $$\   $$\     $$\  $$$$$$\  $$$$$$\  $$$$$$\  
+// $$  __$$\ $$ |  \$$\   $$  |$$  __$$\ \_$$  _|$$  __$$\ 
+// $$ /  $$ |$$ |   \$$\ $$  / $$ /  \__|  $$ |  $$ /  $$ |
+// $$$$$$$$ |$$ |    \$$$$  /  \$$$$$$\    $$ |  $$$$$$$$ |
+// $$  __$$ |$$ |     \$$  /    \____$$\   $$ |  $$  __$$ |
+// $$ |  $$ |$$ |      $$ |    $$\   $$ |  $$ |  $$ |  $$ |
+// $$ |  $$ |$$$$$$$$\ $$ |    \$$$$$$  |$$$$$$\ $$ |  $$ |
+// \__|  \__|\________|\__|     \______/ \______|\__|  \__|
+// 你是否赞同纯美女神爱莉希雅的美貌盖世无双？【愛門】!
+#include <iostream>
+#include <iomanip>
+#include <cstdint>
+#include <climits>
+#include <string>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::pair;
+using std::string;
+using std::vector;
+using std::deque;
+using std::queue;
+using std::stack;
+using std::priority_queue;
+using std::set;
+using std::map;
+using std::sort;
+using std::max_element;
+using std::min_element;
+
+void Solve(void);
+
+int main(void)
+{
+	#ifdef Shimu_Guyue
+		freopen("test.in" , "r", stdin );
+		freopen("test.out", "w", stdout);
+	#else
+	#endif
+
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t = 1;
+	// cin >> t;
+	while (t--)
+	{
+		Solve();
+		// cout << endl << "-------------------------" << endl;
+	}
+	return 0;
+}
+
+void Solve(void)
+{
+	int n;
+	cin >> n;
+	int C = 0;
+	vector<int> lens(n);
+	for (int &len : lens)
+	{
+		cin >> len;
+		C += len;
+	}
+	vector<int64_t> locs(n + 1);
+	locs[0] = 0;
+	for (int i = 1; i <= n; ++i)
+	{
+		locs[i] = locs[i - 1] + lens[i - 1];
+	}
+	for (int i = 0; i < n - 1; ++i)
+	{
+		locs.push_back(locs.back() + lens[i]);
+	}
+
+	int ans = 0;
+	int sum = 0;
+	int r = 1;
+	for (int l = 0; l < n; ++l)
+	{
+		while (sum < C / 2)
+		{
+			sum += locs[r] - locs[r - 1];
+			++r;
+		}
+		ans = std::max(ans, std::min(sum, C - sum));
+		sum -= locs[l + 1] - locs[l];
+	}
+	cout << ans << endl;
+}
