e92140483de4156a4c6652af8091feb92688c5e0
ShimuGuyue <2147539031@qq.com>
2025-02-08 12:49:24 +0800

    2025牛客寒假算法基础集训营4

---
+++ NowCoder/比赛/牛客寒假算法基础集训营/2025牛客寒假算法基础集训营4/I.cpp
@@ -0,0 +1,82 @@
+//  $$$$$$\  $$\   $$\     $$\  $$$$$$\  $$$$$$\  $$$$$$\  
+// $$  __$$\ $$ |  \$$\   $$  |$$  __$$\ \_$$  _|$$  __$$\ 
+// $$ /  $$ |$$ |   \$$\ $$  / $$ /  \__|  $$ |  $$ /  $$ |
+// $$$$$$$$ |$$ |    \$$$$  /  \$$$$$$\    $$ |  $$$$$$$$ |
+// $$  __$$ |$$ |     \$$  /    \____$$\   $$ |  $$  __$$ |
+// $$ |  $$ |$$ |      $$ |    $$\   $$ |  $$ |  $$ |  $$ |
+// $$ |  $$ |$$$$$$$$\ $$ |    \$$$$$$  |$$$$$$\ $$ |  $$ |
+// \__|  \__|\________|\__|     \______/ \______|\__|  \__|
+// 你是否赞同纯美女神爱莉希雅的美貌盖世无双？【愛門】!
+#include <iostream>
+#include <iomanip>
+#include <cstdint>
+#include <climits>
+#include <string>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::pair;
+using std::string;
+using std::vector;
+using std::deque;
+using std::queue;
+using std::stack;
+using std::priority_queue;
+using std::set;
+using std::map;
+using std::sort;
+using std::max_element;
+using std::min_element;
+
+void Solve(void);
+
+int main(void)
+{
+	#ifdef Shimu_Guyue
+		freopen("test.in" , "r", stdin );
+		freopen("test.out", "w", stdout);
+	#else
+	#endif
+
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t = 1;
+	cin >> t;
+	while (t--)
+	{
+		Solve();
+		// cout << endl << "-------------------------" << endl;
+	}
+	return 0;
+}
+
+void Solve(void)
+{
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	int ans = 0;
+	int count_u = 0;
+	for (int i = 0; i < n - 7; ++i)
+	{	//uwawauwa
+		if (s[i]   == 'u' && s[i+1] == 'w' && s[i+2] == 'a' && s[i+3] == 'w' &&
+			s[i+4] == 'a' && s[i+5] == 'u' && s[i+6] == 'w' && s[i+7] == 'a')
+			ans += count_u;
+		if (i - 1 >= 0 && s[i - 1] == 'u')
+			++count_u;
+	}
+	cout << ans << endl;
+}
