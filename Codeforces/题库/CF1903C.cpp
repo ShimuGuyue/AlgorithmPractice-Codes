fc7ca2080fd0aba740d9988fefb2cd841b2d2ce4
ShimuGuyue <2147539031@qq.com>
2025-02-19 18:20:26 +0800

    CF1903C

---
+++ Codeforces/题库/CF1903C.cpp
@@ -0,0 +1,93 @@
+//  $$$$$$\  $$\   $$\     $$\  $$$$$$\  $$$$$$\  $$$$$$\  
+// $$  __$$\ $$ |  \$$\   $$  |$$  __$$\ \_$$  _|$$  __$$\ 
+// $$ /  $$ |$$ |   \$$\ $$  / $$ /  \__|  $$ |  $$ /  $$ |
+// $$$$$$$$ |$$ |    \$$$$  /  \$$$$$$\    $$ |  $$$$$$$$ |
+// $$  __$$ |$$ |     \$$  /    \____$$\   $$ |  $$  __$$ |
+// $$ |  $$ |$$ |      $$ |    $$\   $$ |  $$ |  $$ |  $$ |
+// $$ |  $$ |$$$$$$$$\ $$ |    \$$$$$$  |$$$$$$\ $$ |  $$ |
+// \__|  \__|\________|\__|     \______/ \______|\__|  \__|
+// 您是否赞同纯美女神爱莉希雅的美貌盖世无双？【愛門】!
+#include <iostream>
+#include <iomanip>
+#include <cstdint>
+#include <climits>
+#include <string>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::pair;
+using std::string;
+using std::to_string;
+using std::vector;
+using std::deque;
+using std::queue;
+using std::stack;
+using std::priority_queue;
+using std::set;
+using std::map;
+using std::sort;
+using std::max_element;
+using std::min_element;
+
+/*----------------------------------struct----------------------------------*/
+
+/*-----------------------------------head-----------------------------------*/
+
+void Solve(void);
+
+/*-----------------------------------main-----------------------------------*/
+
+int main(void)
+{
+	#ifdef Shimu_Guyue
+		freopen("test.in" , "r", stdin );
+		freopen("test.out", "w", stdout);
+	#else
+	#endif
+
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t = 1;
+	cin >> t;
+	while (t--)
+	{
+		Solve();
+		// cout << "-------------------------" << endl;
+	}
+	return 0;
+}
+
+/*-----------------------------------body-----------------------------------*/
+
+void Solve(void)
+{
+	int n;
+	cin >> n;
+	vector<int64_t> as(n);
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> as[i];
+	}
+
+	int64_t ans = 0;
+	int64_t sum = 0;
+	for (int i = n - 1; i >= 0; --i)
+	{
+		sum += as[i];
+		if (sum >= 0 || i == 0)
+			ans += sum;
+	}
+	cout << ans << endl;
+}
