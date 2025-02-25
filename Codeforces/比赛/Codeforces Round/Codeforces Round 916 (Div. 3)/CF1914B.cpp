f321aa9dc861519ee6107295049920941054c90b
ShimuGuyue <2147539031@qq.com>
2025-02-25 10:58:39 +0800

    CF1914B

---
+++ Codeforces/比赛/Codeforces Round/Codeforces Round 916 (Div. 3)/CF1914B.cpp
@@ -0,0 +1,93 @@
+//  $$$$$$\  $$\   $$\     $$\  $$$$$$\  $$$$$$\  $$$$$$\  
+// $$  __$$\ $$ |  \$$\   $$  |$$  __$$\ \_$$  _|$$  __$$\ 
+// $$ /  $$ |$$ |   \$$\ $$  / $$ /  \__|  $$ |  $$ /  $$ |
+// $$$$$$$$ |$$ |    \$$$$  /  \$$$$$$\    $$ |  $$$$$$$$ |
+// $$  __$$ |$$ |     \$$  /    \____$$\   $$ |  $$  __$$ |
+// $$ |  $$ |$$ |      $$ |    $$\   $$ |  $$ |  $$ |  $$ |
+// $$ |  $$ |$$$$$$$$\ $$ |    \$$$$$$  |$$$$$$\ $$ |  $$ |
+// \__|  \__|\________|\__|     \______/ \______|\__|  \__|
+// 您是否赞同纯美女神爱莉希雅的美貌盖世无双？【愛門】!
+#include <iostream>
+#include <iomanip>
+#include <cstdint>
+#include <climits>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <bitset>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::pair;
+using std::string;
+using std::to_string;
+using std::stoi;
+using std::vector;
+using std::deque;
+using std::queue;
+using std::stack;
+using std::priority_queue;
+using std::set;
+using std::map;
+using std::bitset;
+using std::lower_bound;
+using std::upper_bound;
+using std::sort;
+using std::max_element;
+using std::min_element;
+
+/*----------------------------------struct----------------------------------*/
+
+/*-----------------------------------head-----------------------------------*/
+
+void Solve(void);
+
+/*-----------------------------------main-----------------------------------*/
+
+int main(void)
+{
+	#ifdef Shimu_Guyue
+		freopen("test.in" , "r", stdin );
+		freopen("test.out", "w", stdout);
+	#else
+	#endif
+
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t = 1;
+	cin >> t;
+	while (t--)
+	{ 
+		Solve();
+		// cout << "-------------------------" << endl;
+	}
+	return 0;
+}
+
+/*-----------------------------------body-----------------------------------*/
+
+void Solve(void)
+{
+	int n, k;
+	cin >> n >> k;
+	for (int i = n - k; i <= n; ++i)
+	{
+		cout << i << " ";
+	}
+	for (int i = n - k - 1; i > 0; --i)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+}
