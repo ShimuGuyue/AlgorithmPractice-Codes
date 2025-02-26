139168125e62597ae42292c0438b753321f890bf
ShimuGuyue <2147539031@qq.com>
2025-02-26 18:26:52 +0800

    CF1914E

---
+++ Codeforces/题库/CF1914E.cpp
@@ -0,0 +1,129 @@
+//  $$$$$$\  $$\   $$\     $$\  $$$$$$\  $$$$$$\  $$$$$$\  
+// $$  __$$\ $$ |  \$$\   $$  |$$  __$$\ \_$$  _|$$  __$$\ 
+// $$ /  $$ |$$ |   \$$\ $$  / $$ /  \__|  $$ |  $$ /  $$ |
+// $$$$$$$$ |$$ |    \$$$$  /  \$$$$$$\    $$ |  $$$$$$$$ |
+// $$  __$$ |$$ |     \$$  /    \____$$\   $$ |  $$  __$$ |
+// $$ |  $$ |$$ |      $$ |    $$\   $$ |  $$ |  $$ |  $$ |
+// $$ |  $$ |$$$$$$$$\ $$ |    \$$$$$$  |$$$$$$\ $$ |  $$ |
+// \__|  \__|\________|\__|     \______/ \______|\__|  \__|
+// 您是否赞同纯美女神爱莉希雅的美貌盖世无双？【愛門】!
+#include <iostream>
+#include <iomanip>
+#include <cstdint>
+#include <climits>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <bitset>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::pair;
+using std::string;
+using std::to_string;
+using std::stoi;
+using std::vector;
+using std::deque;
+using std::queue;
+using std::stack;
+using std::priority_queue;
+using std::set;
+using std::map;
+using std::bitset;
+using std::lower_bound;
+using std::upper_bound;
+using std::sort;
+using std::max_element;
+using std::min_element;
+
+template <typename T>
+std::istream& operator>>(std::istream &in, vector<T> &v)
+{
+	for (T &t : v) in >> t;
+	return in;
+}
+template <typename T>
+std::ostream& operator<<(std::ostream &out, vector<T> &v)
+{
+	for (T &t : v) out << t << " ";
+	return out;
+}
+
+/*----------------------------------struct----------------------------------*/
+
+/*-----------------------------------head-----------------------------------*/
+
+void Solve(void);
+
+/*-----------------------------------main-----------------------------------*/
+
+int main(void)
+{
+	#ifdef Shimu_Guyue
+		freopen("test.in" , "r", stdin );
+		freopen("test.out", "w", stdout);
+	#else
+	#endif
+
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t = 1;
+	cin >> t;
+	while (t--)
+	{ 
+		Solve();
+		// cout << "-------------------------" << endl;
+	}
+	return 0;
+}
+
+/*-----------------------------------body-----------------------------------*/
+
+void Solve(void)
+{
+	int n;
+	cin >> n;
+	vector<int> as(n);
+	vector<int> bs(n);
+	for (auto &a : as)
+	{
+		cin >> a;
+	}
+	for (auto &b : bs)
+	{
+		cin >> b;
+	}
+
+	vector<pair<int, int>> values(n);
+	for (int i = 0; i < n; ++i)
+	{
+		values[i].first = as[i] + bs[i] - 1;
+		values[i].second = i;
+	}
+	sort(values.begin(), values.end(),
+	[](pair<int, int> a, pair<int, int> b)
+	{
+		return a.first > b.first;
+	});
+
+	int64_t ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (i & 1)
+			ans = ans - bs[values[i].second] + 1;
+		else
+			ans = ans + as[values[i].second] - 1;
+	}
+	cout << ans << endl;
+}
