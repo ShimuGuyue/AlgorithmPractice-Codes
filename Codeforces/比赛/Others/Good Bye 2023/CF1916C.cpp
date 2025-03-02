1b2710d40b23fa2ec3862deaf54a6264ebbfc45e
ShimuGuyue <2147539031@qq.com>
2025-03-02 13:45:54 +0800

    CF 1916C

---
+++ Codeforces/比赛/Others/Good Bye 2023/CF1916C.cpp
@@ -0,0 +1,138 @@
+/*
+/$$$$$$\  $$\   $$\     $$\  $$$$$$\  $$$$$$\  $$$$$$\  
+$$  __$$\ $$ |  \$$\   $$  |$$  __$$\ \_$$  _|$$  __$$\ 
+$$ /  $$ |$$ |   \$$\ $$  / $$ /  \__|  $$ |  $$ /  $$ |
+$$$$$$$$ |$$ |    \$$$$  /  \$$$$$$\    $$ |  $$$$$$$$ |
+$$  __$$ |$$ |     \$$  /    \____$$\   $$ |  $$  __$$ |
+$$ |  $$ |$$ |      $$ |    $$\    $$ |  $$ |  $$ |  $$ |
+$$ |  $$ |$$$$$$$$\ $$ |    \$$$$$$  |$$$$$$\ $$ |  $$ |
+\__|  \__|\________|\__|     \______/ \______|\__|  \__|
+您是否赞同纯美女神爱莉希雅的美貌盖世无双？【愛門】!
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdint>
+#include <climits>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <bitset>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::pair;
+using std::string;
+using std::to_string;
+using std::stoi;
+using std::vector;
+using std::deque;
+using std::list;
+using std::queue;
+using std::stack;
+using std::priority_queue;
+using std::set;
+using std::map;
+using std::bitset;
+using std::lower_bound;
+using std::upper_bound;
+using std::sort;
+using std::max_element;
+using std::min_element;
+
+template <typename T>
+std::istream& operator>>(std::istream &in, vector<T> &v)
+{
+	for (T &t : v) in >> t;
+	return in;
+}
+template <typename T>
+std::ostream& operator<<(std::ostream &out, vector<T> &v)
+{
+	for (T &t : v) out << t << " ";
+	return out;
+}
+
+/*----------------------------------struct----------------------------------*/
+
+/*-----------------------------------head-----------------------------------*/
+
+void Solve(void);
+
+/*-----------------------------------main-----------------------------------*/
+
+int main(void)
+{
+	#ifdef Shimu_Guyue
+		freopen("test.in" , "r", stdin );
+		freopen("test.out", "w", stdout);
+	#else
+	#endif
+
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+	#define endl '\n'
+
+    int t = 1;
+	cin >> t;
+	while (t--)
+	{ 
+		Solve();
+		// cout << "-------------------------" << endl;
+	}
+	return 0;
+}
+
+/*-----------------------------------body-----------------------------------*/
+
+void Solve(void)
+{
+	int n;
+	cin >> n;
+	vector<int> as(n);
+	cin >> as;
+	vector<int64_t> sums(n);
+	sums[0] = as[0];
+	for (int i = 1; i < n; ++i)
+	{
+		sums[i] = sums[i - 1] + as[i];
+	}
+
+	vector<int> count_odds(n);
+	count_odds[0] = (as[0] & 1);
+	for (int i = 1; i < n; ++i)
+	{
+		count_odds[i] = count_odds[i - 1] + (as[i] & 1);
+	}
+
+	vector<int> cost_values(n);
+	cost_values[0] = 0;
+	for (int i = 1; i < n; ++i)
+	{
+		if (count_odds[i] == 0)
+			cost_values[i] = 0;
+		else
+		if (count_odds[i] == 1)
+			cost_values[i] = 1;
+		else
+			cost_values[i] = (count_odds[i] - 2) / 3 + (count_odds[i] + 1) % 3;
+
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		cout << sums[i] - cost_values[i] << " ";
+	}
+	cout << endl;
+}
