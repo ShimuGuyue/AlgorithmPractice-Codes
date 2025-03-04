4e36271c19f79f2a1b6c2267f8afaff336e63ab0
ShimuGuyue <2147539031@qq.com>
2025-03-04 14:50:53 +0800

    CF1918C

---
+++ Codeforces/比赛/Codeforces Round/Codeforces Round 922 (Div. 2)/CF1918C.cpp
@@ -0,0 +1,129 @@
+/*
+/$$$$$$\  $$\   $$\     $$\  $$$$$$\  $$$$$$\  $$$$$$\  
+$$  __$$\ $$ |  \$$\   $$  |$$  __$$\ \_$$  _|$$  __$$\ 
+$$ /  $$ |$$ |   \$$\ $$  / $$ /  \__|  $$ |  $$ /  $$ |
+$$$$$$$$ |$$ |    \$$$$  /  \$$$$$$\    $$ |  $$$$$$$$ |
+$$  __$$ |$$ |     \$$  /    \____$$\   $$ |  $$  __$$ |
+$$ |  $$ |$$ |      $$ |    $$\    $$|  $$ |  $$ |  $$ |
+$$ |  $$ |$$$$$$$$\ $$ |    \$$$$$$  |$$$$$$\ $$ |  $$ |
+\__|  \__|\________|\__|     \______/ \______|\__|  \__|
+您是否赞同纯美女神爱莉希雅的美貌盖世无双？【愛門】!
+*/
+#pragma region
+#include <iostream>
+#include <iomanip>
+#include <cstdint>
+#include <climits>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <bitset>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::pair;
+using std::string;
+using std::to_string;
+using std::stoi;
+using std::vector;
+using std::deque;
+using std::list;
+using std::queue;
+using std::stack;
+using std::priority_queue;
+using std::set;
+using std::map;
+using std::bitset;
+using std::lower_bound;
+using std::upper_bound;
+using std::sort;
+using std::max_element;
+using std::min_element;
+
+template <typename T>
+std::istream& operator>>(std::istream &in, vector<T> &v)
+{
+	for (T &t : v) in >> t;
+	return in;
+}
+template <typename T>
+std::ostream& operator<<(std::ostream &out, vector<T> &v)
+{
+	for (T &t : v) out << t << " ";
+	return out;
+}
+#pragma endregion
+/*----------------------------------struct----------------------------------*/
+
+/*-----------------------------------head-----------------------------------*/
+
+void Solve(void);
+
+/*-----------------------------------main-----------------------------------*/
+
+int main(void)
+{
+#pragma region
+	#ifdef Shimu_Guyue
+		freopen("test.in" , "r", stdin );
+		freopen("test.out", "w", stdout);
+	#else
+	#endif
+
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+	#define endl '\n'
+#pragma endregion
+
+    int t = 1;
+	cin >> t;
+	while (t--)
+	{ 
+		Solve();
+		// cout << "-------------------------" << endl;
+	}
+	return 0;
+}
+/*-----------------------------------body-----------------------------------*/
+
+void Solve(void)
+{
+	int64_t a, b, r;
+	cin >> a >> b >> r;
+
+	bitset<64> aa(a);
+	bitset<64> bb(b);
+	bitset<64> x;
+	bool turn = false;
+	for (int i = 63; i >= 0; --i)
+	{
+		if (aa[i] == bb[i])
+			continue;
+		if ((aa.to_ullong() > bb.to_ullong() && aa[i] == 1 && bb[i] == 0)
+		||  (aa.to_ullong() < bb.to_ullong() && aa[i] == 0 && bb[i] == 1))
+		{
+			if (turn == false)
+			{
+				turn = true;
+				continue;
+			}
+			x[i] = 1;
+			if (x.to_ullong() > r)
+				x[i] = 0;
+			aa[i] = aa[i] ^ x[i];
+			bb[i] = bb[i] ^ x[i];
+		}
+	}
+	cout << llabs(aa.to_ullong() - bb.to_ullong()) << endl;
+}
