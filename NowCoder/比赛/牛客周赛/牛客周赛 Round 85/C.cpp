e0c11c705b711663492adf60a09c008326acf28d
ShimuGuyue <2147539031@qq.com>
2025-03-22 15:32:27 +0800

    牛客周赛 Round 85 ABCDEF

---
+++ NowCoder/比赛/牛客周赛/牛客周赛 Round 85/C.cpp
@@ -0,0 +1,115 @@
+/*
+/$$$$$$\  $$\   $$\     $$\  $$$$$$\  $$$$$$\  $$$$$$\  
+$$  __$$\ $$ |  \$$\   $$  |$$  __$$\ \_$$  _|$$  __$$\ 
+$$ /  $$ |$$ |   \$$\ $$  / $$ /  \__|  $$ |  $$ /  $$ |
+$$$$$$$$ |$$ |    \$$$$  /  \$$$$$$\    $$ |  $$$$$$$$ |
+$$  __$$ |$$ |     \$$  /    \____$$\   $$ |  $$  __$$ |
+$$ |  $$ |$$ |      $$ |    $$\    $$|  $$ |  $$ |  $$ |
+$$ |  $$ |$$$$$$$$\ $$ |    \$$$$$$  |$$$$$$\ $$ |  $$ |
+\__|  \__|\________|\__|     \______/ \______|\__|  \__|
+您是否赞同纯美女神爱莉希雅的美貌盖世无双？【愛門】!
+*/
+
+#pragma region
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+#include <iomanip>
+using std::fixed;
+using std::setprecision;
+using std::setw;
+#include <cstdint>
+#include <climits>
+#include <cmath>
+#include <utility>
+using std::pair;
+#include <string>
+using std::string;
+using std::to_string;
+using std::stoi;
+#include <vector>
+using std::vector;
+#include <deque>
+using std::deque;
+#include <list>
+using std::list;
+#include <stack>
+using std::stack;
+#include <queue>
+using std::queue;
+using std::priority_queue;
+#include <set>
+using std::set;
+#include <map>
+using std::map;
+#include <bitset>
+using std::bitset;
+#include <algorithm>
+using std::lower_bound;
+using std::upper_bound;
+using std::sort;
+using std::max_element;
+using std::min_element;
+
+template <typename T>
+std::istream& operator>>(std::istream &in, vector<T> &v)
+{
+	for (T &t : v) in >> t;
+	return in;
+}
+template <typename T>
+std::ostream& operator<<(std::ostream &out, vector<T> &v)
+{
+	for (T &t : v) out << t << " ";
+	return out;
+}
+#pragma endregion
+/*----------------------------------struct----------------------------------*/
+
+/*-----------------------------------head-----------------------------------*/
+
+void Solve(void);
+
+/*-----------------------------------main-----------------------------------*/
+
+int main(void)
+{
+#pragma region
+	#ifdef Shimu_Guyue
+		freopen("test.in" , "r", stdin );
+		freopen("test.out", "w", stdout);
+	#else
+	#endif
+
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+	#define endl '\n'
+#pragma endregion
+
+    int t = 1;
+	cin >> t;
+	// cout << endl;
+	while (t--)
+	{ 
+		Solve();
+		// cout << "-------------------------" << endl;
+	}
+	return 0;
+}
+/*-----------------------------------body-----------------------------------*/
+
+void Solve(void)
+{
+	string s;
+	cin >> s;
+	int n = s.size();
+	int flag = 0;
+	for (int i = 1; i < n; ++i)
+	{
+		if (s[i] != s[i - 1])
+			++flag;
+	}
+	cout << (flag <= 4 ? "Yes" : "No") << endl;
+} 
