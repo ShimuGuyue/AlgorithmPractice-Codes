976917637173dc541cfffa60a0f4a4f06c2285ee
ShimuGuyue <2147539031@qq.com>
2025-04-26 22:43:35 +0800

    Create NC226498.cpp

---
+++ NowCoder/题库/NC226498.cpp
@@ -0,0 +1,168 @@
+#pragma region
+/*
+/$$$$$$\  $$\   $$\     $$\  $$$$$$\  $$$$$$\  $$$$$$\  
+$$  __$$\ $$ |  \$$\   $$  |$$  __$$\ \_$$  _|$$  __$$\ 
+$$ /  $$ |$$ |   \$$\ $$  / $$ /  \__|  $$ |  $$ /  $$ |
+$$$$$$$$ |$$ |    \$$$$  /  \$$$$$$\    $$ |  $$$$$$$$ |
+$$  __$$ |$$ |     \$$  /    \____$$\   $$ |  $$  __$$ |
+$$ |  $$ |$$ |      $$ |    $$\    $$|  $$ |  $$ |  $$ |
+$$ |  $$ |$$$$$$$$\ $$ |    \$$$$$$  |$$$$$$\ $$ |  $$ |
+\__|  \__|\________|\__|     \______/ \______|\__|  \__|
+您是否赞同纯美女神爱莉希雅的美貌盖世无双？【愛門】!
+*/
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+#include <iomanip>
+using std::fixed;
+using std::setprecision;
+using std::setw;
+#include <cstdint>
+#include <climits>
+#include <cmath>
+#include <utility>
+using std::pair;
+#include <string>
+using std::string;
+using std::to_string;
+using std::stoi;
+#include <vector>
+using std::vector;
+#include <deque>
+using std::deque;
+#include <list>
+using std::list;
+#include <stack>
+using std::stack;
+#include <queue>
+using std::queue;
+using std::priority_queue;
+#include <set>
+using std::set;
+#include <map>
+using std::map;
+#include <bitset>
+using std::bitset;
+#include <algorithm>
+using std::lower_bound;
+using std::upper_bound;
+using std::sort;
+using std::max_element;
+using std::min_element;
+using std::next_permutation;
+#include <random>
+
+std::mt19937 Rand(time(nullptr));
+
+template <typename T, typename K>
+std::istream& operator>>(std::istream &in, pair<T, K> &p)
+{
+	in >> p.first >> p.second;
+	return in;
+}
+template <typename T, typename K>
+std::ostream& operator<<(std::ostream &out, pair<T, K> &p)
+{
+	out << p.first << ' ' << p.second;
+	return out;
+}
+template <typename T>
+std::istream& operator>>(std::istream &in, vector<T> &v)
+{
+	for (T &t : v) in >> t;
+	return in;
+}
+template <typename T>
+std::ostream& operator<<(std::ostream &out, vector<T> &v)
+{
+	for (T &t : v) out << t << ' ';
+	return out;
+}
+#pragma endregion
+
+/*----------------------------------solve-----------------------------------*/
+
+class HYS
+{
+public:
+	static void Solve(void)
+	{
+		int n, m;
+		cin >> n >> m;
+		vector<map<int, int>> grid(5000 + 1);
+		for (int i = 0; i < m; ++i)
+		{
+			int u, v, w;
+			cin >> u >> v >> w;
+			grid[u][v] = w;
+			grid[v][u] = w;
+		}
+
+		struct Node
+		{
+			int node;
+			int64_t len;
+
+			bool operator>(const Node &o) const
+			{
+				return this->len > o.len;
+			}
+		};
+		priority_queue<Node, vector<Node>, std::greater<Node>> h;
+		vector<int64_t> distances(5000 + 1, INT64_MAX);
+		vector<int> visited(5000 + 1, false);
+		distances[1] = 0;
+		h.push({1, 0});
+		while (!h.empty())
+		{
+			int u = h.top().node;
+			int64_t l = h.top().len;
+			h.pop();
+			if (visited[u])
+				continue;
+			visited[u] = true;
+
+			for (auto vw : grid[u])
+			{
+				int v = vw.first;
+				int w = vw.second;
+				if (visited[v])
+					continue;
+				if (l + w < distances[v])
+				{
+					distances[v] = l + w;
+					h.push({v, distances[v]});
+				}
+			}
+		}
+		cout << (visited[n] ? distances[n] : -1) << endl;
+	}
+
+	// static 
+
+	// static 
+
+	// static 
+};
+
+/*-----------------------------------main-----------------------------------*/
+
+int main(void)
+{
+	#ifdef Shimu_Guyue
+		freopen( "in.test", "r", stdin );
+		freopen("out.test", "w", stdout);
+	#endif
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+	// cin >> t;
+	while (t--)
+	{ 
+		HYS::Solve();
+		// cout << "-------------------------" << endl;
+	}
+	return 0;
+}
