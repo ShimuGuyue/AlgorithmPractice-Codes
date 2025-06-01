dfb954009f1d53324960666675f2a2ba08b323ec
ShimuGuyue <2147539031@qq.com>
2025-06-01 14:01:12 +0800

    AtCoder Beginner Contest 408 {A，B，C，D，E}

---
+++ AtCoder/题库/ABC408C.cpp
@@ -0,0 +1,342 @@
+/*-----------------------------------head-----------------------------------*/
+#pragma region template
+
+/*
+/$$$$$$\  $$\   $$\     $$\  $$$$$$\  $$$$$$\  $$$$$$\
+$$  __$$\ $$ |  \$$\   $$  |$$  __$$\ \_$$  _|$$  __$$\
+$$ /  $$ |$$ |   \$$\ $$  / $$ /  \__|  $$ |  $$ /  $$ |
+$$$$$$$$ |$$ |    \$$$$  /  \$$$$$$\    $$ |  $$$$$$$$ |
+$$  __$$ |$$ |     \$$  /    \____$$\   $$ |  $$  __$$ |
+$$ |  $$ |$$ |      $$ |    $$\    $$|  $$ |  $$ |  $$ |
+$$ |  $$ |$$$$$$$$\ $$ |    \$$$$$$  |$$$$$$\ $$ |  $$ |
+\__|  \__|\________|\__|     \______/ \______|\__|  \__|
+您是否赞同纯美女神爱莉希雅的美貌盖世无双？【愛門】!
+*/
+
+#pragma region HeadFile
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+#include <sstream>
+using std::stringstream;
+#include <iomanip>
+using std::fixed;
+using std::setprecision;
+using std::setw;
+#include <cstdint>
+#include <climits>
+#include <cmath>
+#include <utility>
+using std::pair;
+#include <string>
+using std::string;
+using std::to_string;
+using std::stoi;
+#include <vector>
+using std::vector;
+#include <deque>
+using std::deque;
+#include <list>
+using std::list;
+#include <stack>
+using std::stack;
+#include <queue>
+using std::queue;
+using std::priority_queue;
+#include <set>
+using std::set;
+#include <map>
+using std::map;
+#include <iterator>
+using std::begin;
+using std::end;
+#include <bitset>
+using std::bitset;
+#include <algorithm>
+using std::sort;
+#include <random>
+
+#pragma endregion
+
+#pragma region Debug
+
+std::string Debug(const int32_t &i)
+{
+	std::stringstream ss;
+	ss << i;
+	return ss.str();
+}
+std::string Debug(const int64_t &i)
+{
+	std::stringstream ss;
+	ss << i;
+	return ss.str();
+}
+
+std::string Debug(const double &f)
+{
+	std::stringstream ss;
+	ss << std::fixed << std::setprecision(12) << f;
+	return ss.str();
+}
+
+std::string Debug(const char &c)
+{
+	std::stringstream ss;
+	ss << '\'' << c << '\'';
+	return ss.str();
+}
+
+std::string Debug(const bool &b)
+{
+	std::stringstream ss;
+	ss << (b ? "true" : "false");
+	return ss.str();
+}
+
+std::string Debug(const std::string &s)
+{
+	std::stringstream ss;
+	ss << "\"" << s << "\"";
+	return ss.str();
+}
+
+template <typename T, typename K>
+std::string Debug(const std::pair<T, K> &p)
+{
+	std::stringstream ss;
+	ss << "[" << Debug(p.first) << ", " << Debug(p.second) << "]";
+	return ss.str();
+}
+
+template <typename T>
+std::string Debug(const T& t)
+{
+	std::stringstream ss;
+	ss << "{";
+	bool first = true;
+	for (const auto &a : t)
+	{
+		if (!first)
+			ss << ", ";
+		first = false;
+		ss << Debug(a);
+	}
+	ss << "}";
+	return ss.str();
+}
+
+#pragma endregion
+
+#pragma region IO
+
+template <typename T, typename K>
+std::istream& operator>>(std::istream &in, std::pair<T, K> &p)
+{
+	in >> p.first >> p.second;
+	return in;
+}
+template <typename T, typename K>
+std::ostream& operator<<(std::ostream &out, const std::pair<T, K> &p)
+{
+	out << p.first << ' ' << p.second;
+	return out;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &in, std::vector<T> &v)
+{
+	for (T &t : v) in >> t;
+	return in;
+}
+template <typename T>
+std::ostream& operator<<(std::ostream &out, const std::vector<T> &v)
+{
+	for (const T &t : v) out << t << ' ';
+	return out;
+}
+template <typename T>
+std::ostream& operator>>(std::ostream &out, const std::vector<T> &v)
+{
+	for (auto rit = v.rbegin(); rit != v.rend(); ++rit)
+		out << *rit << ' ';
+	return out;
+}
+
+#pragma endregion
+
+#pragma region Struct
+
+struct Edge
+{
+	int u, v, w;
+
+	bool operator<(const Edge &o) const
+	{
+		return this->w < o.w;
+	}
+	bool operator>(const Edge &o) const
+	{
+		return this->w > o.w;
+	}
+};
+
+struct DisjointSet
+{
+	vector<int> sets;
+	vector<int> ranks;
+
+	DisjointSet()
+	{}
+	DisjointSet(const int &n) : sets(n), ranks(n, 1)
+	{
+		for (int i = 0; i < n; ++i)
+		{
+			sets[i] = i;
+		}
+	}
+
+	int Find_set(const int &x)
+	{
+		return sets[x] == x ? x : sets[x] = Find_set(sets[x]);
+	}
+
+	void Merge_set(const int &x, const int &y)
+	{
+		int set_x = Find_set(x);
+		int set_y = Find_set(y);
+		if (ranks[set_x] < ranks[set_y])
+		{
+			sets[set_x] = set_y;
+		}
+		else
+		{
+			sets[set_y] = set_x;
+			if (ranks[set_x] == ranks[set_y])
+				++ranks[set_x];
+		}
+	}
+
+	bool In_same_set(const int &x, const int &y)
+	{
+		return Find_set(x) == Find_set(y);
+	}
+
+	int Count_sets()
+	{
+		set<int> count;
+		for (int i = 0; i < sets.size(); ++i)
+		{
+			count.insert(Find_set(i));
+		}
+		return count.size();
+	}
+};
+
+#pragma endregion
+
+#pragma region Function
+
+int64_t Pow(int base, int n)
+{
+	int64_t ans = 1;
+	while (n)
+	{
+		if (n & 1)
+			ans *= base;
+		base *= base;
+		n >>= 1;
+	}
+	return ans;
+}
+
+int64_t Pow(int base, int n, int mod)
+{
+	int64_t ans = 1;
+	while (n)
+	{
+		if (n & 1)
+			ans = ans * base % mod;
+		base = base * base % mod;
+		n >>= 1;
+	}
+	return ans;
+}
+
+int64_t Ceil(int64_t a, int64_t b)
+{
+	return a / b + (a % b != 0);
+}
+
+#pragma endregion
+
+#pragma region Random
+
+std::mt19937 Rand(time(nullptr));
+
+#pragma endregion
+
+#pragma region Nickname
+
+template <typename T>
+using MaxHeap = std::priority_queue<T, std::vector<T>, std::less<T>>;
+template <typename T>
+using MinHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#pragma endregion
+
+#pragma endregion
+
+/*-----------------------------------body-----------------------------------*/
+class ShimuGuyue
+{
+public:
+	static const int mod = 1e9+7;
+public:
+	static void Solve()
+	{
+		int n, m;
+		cin >> n >> m;
+		vector<int> difs(n + 2);
+		while (m--)
+		{
+			int l, r;
+			cin >> l >> r;
+			++difs[l];
+			--difs[r + 1];
+		}
+		vector<int> as(n + 1, 0);
+		for (int i = 1; i <= n; ++i)
+		{
+			as[i] = as[i - 1] + difs[i];
+		}
+		int ans = *std::min_element(as.begin() + 1, as.end());
+		cout << ans << endl;
+	}
+
+	// static 
+
+	// static 
+};
+
+/*-----------------------------------main-----------------------------------*/
+int main()
+{
+	#ifdef Shimu_Guyue
+		freopen(".test/test.in" , "r", stdin );
+		freopen(".test/test.out", "w", stdout);
+	#endif
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+	// cin >> t;
+	while (t--)
+	{
+		ShimuGuyue::Solve();
+		// cout << "-------------------------" << endl;
+	}
+	return 0;
+}
